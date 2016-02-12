21a1f8ff2747d4f7b138f05b47a133bfb2caa331
Peter Williams <peter.williams@papercut.com>
2016-02-13 10:30:08 +1100

    Removed outer loop from speed test function

--- src/speed-tests/suffix-array.go
+++ src/speed-tests/suffix-array.go
@@ -2,115 +2,101 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"math/rand"
 	"index/suffixarray"
+	"math/rand"
+	"time"
 )
 
-
-func sum(arr[] int) int {
+// Returns: Sum of elements in `arr`
+func sum(arr []int) int {
 	s := 0
-	for i:= 0; i < len(arr); i++ {
+	for i := 0; i < len(arr); i++ {
 		s += arr[i]
 	}
 	return s
 }
 
-func test_sa_speed() {
+func test_sa_speed(P int) {
 	const (
-		N = 2e7 	// Max size of text
-		M = 200  	// Size of repeated substrings in text
-		T = 1e9		// Size of text x number of test runs
-		R = 2		// 1 / R = fraction of text made up of pattern repetitions
+		T = 2e7 // Max size of text
+		// P = 100  	// Size of repeated substrings in text
+		L = 1e9 // (Size of text) x (number of test runs)
+		R = 2   // 1 / R = fraction of text made up of pattern repetitions
 	)
 
-	fmt.Printf("Test suffix array speed: text=%e chars, pattern=%d chars\n", N, M)
+	fmt.Printf("Test suffix array speed: text=%e chars, pattern=%d chars, %e matches\n",
+		T, P, T/R)
 
-	text := make([]byte, N, N) 			// String to be indexed and searched
-	pattern := make([]byte, M, M) 		// Substring to search for
+	text := make([]byte, T, T)    // String to be indexed and searched
+	pattern := make([]byte, P, P) // Substring to search for
 
-	for j := 0; j < M; j++ {
+	for j := 0; j < P; j++ {
 		pattern[j] = byte(j)
 	}
-	for i := 0; i < N; i++ {
-		text[i] = pattern[i % M]
+	for i := 0; i < T; i++ {
+		text[i] = pattern[i%P]
 	}
-	for i := M; i <= N - (R - 1) * M; i += R * M {
-		j := int(rand.Int31n(M - 1))
+	for i := P; i <= T-(R-1)*P; i += R * P {
+		j := int(rand.Int31n(int32(P) - 1))
 		k := 1 + byte(rand.Int31n(0xFE))
-		text[i + j] +=  k
+		text[i+j] += k
 	}
 
-	di := -1
-	var dvp, dvt byte
-	for j := 0; j < M; j++ {
-		if pattern[j] != text[M + j] {
-			di = j
-			dvp = pattern[j]
-			dvt = text[M + j]
-			break
+	// di := -1
+	// var dvp, dvt byte
+	// for j := 0; j < P; j++ {
+	// 	if pattern[j] != text[P + j] {
+	// 		di = j
+	// 		dvp = pattern[j]
+	// 		dvt = text[P + j]
+	// 		break
+	// 	}
+	// }
+
+	// fmt.Printf("diff: j=%d,dvp=%d,dvt=%d\n", di, dvp, dvt)
+	// fmt.Println(pattern[:10])
+	// fmt.Println(text[P:P + 10])
+
+	for n := P * R; n <= T; n *= 10 {
+		fmt.Printf("%9d", n)
+		//start0 := time.Now()
+		text_index := suffixarray.New(text[:n])
+		fmt.Printf(": ")
+		n_repeats := L / n // number of test repeats
+		if n_repeats < 1 {
+			n_repeats = 1
 		}
-	}
 
-	fmt.Printf("diff: j=%d,dvp=%d,dvt=%d\n", di, dvp, dvt)
-	fmt.Println(pattern[:10])
-	fmt.Println(text[M:M + 10])
-
-	for q := 0; q < 3; q++ {
-		for n := M; n <= N; n *= 10 {
-			fmt.Printf("$ %9d", n)
-			start0 := time.Now()
-			text_index := suffixarray.New(text[:n])
-			fmt.Printf(": ")
-			t := T / n // number of test repeats
-			if t < 1 {
-				t = 1
-			}
-
-			matches := []int{} // number of pattern matches
-			start := time.Now()
-			for i := 0; i < t; i++ {
-				offsets := text_index.Lookup(pattern, -1)
-				matches = append(matches, len(offsets))
-			}
-
-			duration_total := time.Since(start0).Seconds()
-			duration_all := time.Since(start).Seconds() // Duration of all t tests
-			duration := duration_all                    // Duration per pattern match
-			n_matches := sum(matches) / len(matches)
-			if n_matches > 0.0 {
-				duration /= float64(n_matches)
-			}
-			period_all := duration_all / float64(t) // period
-			period := duration / float64(t)
-
-			fmt.Printf("%e %e (%e %9d %7d %e %e)\n", period, period_all, duration, t, n_matches,
-				duration_all, duration_total)
+		matches := []int{} // number of pattern matches
+		start := time.Now()
+		for i := 0; i < n_repeats; i++ {
+			offsets := text_index.Lookup(pattern, -1)
+			matches = append(matches, len(offsets))
 		}
-		fmt.Println()
+
+		//duration_total := time.Since(start0).Seconds()
+
+		duration := time.Since(start).Seconds() / float64(n_repeats)
+		//duration := time.Since(start).Seconds() / float64(n_repeats) // Duration per repeat
+
+		n_matches := sum(matches) / len(matches)
+		if n_matches*R*P != n {
+			msg := fmt.Sprintf("Unexpected number of matches: n_matches=%d, n / (R * P)=%d (%d / %d %d)",
+				n_matches, n/(R*P), sum(matches), len(matches), n_repeats)
+			panic(msg)
+		}
+
+		duration_match := duration / float64(n_matches) // Duration per pattern match
+		duration_char := duration_match / float64(P)
+
+		fmt.Printf("%7d %e %e %e %e\n", n_matches, duration, duration_match, duration_char,
+			duration/float64(n))
+		//duration_all, duration_total)
 	}
 }
 
 func main() {
-	// fmt.Println("hello, Mr. w0rld!")
-	// a := 0
-	// b := 1
-	// if a+1 == b {
-	// 	fmt.Printf("Hello again, a=%d\n", a)
-	// }
-
-	// fmt.Printf("program=%s\n", os.Args[0])
-
-	// test_coord()
-	// test_array()
-	// test_copy()
-	// test_map(2)
-	// test_map(3)
-	// test_sqrt()
-	// //test_io()
-	// test_types()
-	// test_sa()
-	for {
-		test_sa_speed()
+	for P := 10; P <= 10*1000; P += 10 {
+		test_sa_speed(P)
 	}
 }
